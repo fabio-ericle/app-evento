0b9a28e8882ff4bdd847a54816ae845de8237584
fabio-ericle <fabioericle@gmail.com>
2022-01-09 23:50:53 -0300

    Update user.routes.ts

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import { userAuthenticated } from '../middlewares/userAuthenticated';
 import { UserController } from '../resources/user/user.controller';
 
 const userRouter = Router();
 const userController = new UserController();
 
 userRouter.post("/signin", userController.signUp);
+userRouter.post("/signin", userAuthenticated, userController.signIn);
 
 export default  userRouter;
