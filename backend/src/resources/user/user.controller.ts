a163528bcbfe121569fdd9d1cbbc05158ea67fd0
fabio-ericle <fabioericle@gmail.com>
2022-01-09 23:25:24 -0300

    Update user.controller.ts

--- backend/src/resources/user/user.controller.ts
+++ backend/src/resources/user/user.controller.ts
@@ -2,10 +2,10 @@ import { Request, Response } from 'express';
 import { UserServices } from './user.service';
 
 export class UserController {
-   async signin(request: Request, response: Response) {
+   async signUp(request: Request, response: Response) {
       const { name = '', email = '', password = '' } = request.body;
       const userService = new UserServices();
-      const user = await userService.signin({ name, email, password });
+      const user = await userService.signUp({ name, email, password });
 
       return response.status(201).send(user);
    }
