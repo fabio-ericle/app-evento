3168cd9e3d4bfb573acb303fe73f5f3a8f34fd6f
fabio-ericle <fabioericle@gmail.com>
2022-01-19 00:31:17 -0300

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,12 +9,11 @@ const PORT = process.env.PORT ?? '3001';
 
 createConnection().then(
    connection => {
-      console.log("Passou",PORT);
       const app = express();
 
       app.use(cors());
       app.use(express.json());
-      // app.use(routes);
+      app.use(routes);
       app.use(globalErrors)
 
       app.listen(PORT, () => {
