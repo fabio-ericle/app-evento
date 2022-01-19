a2ec25a6c7f7968278a027d24ba96f148e50be2c
fabio-ericle <fabioericle@gmail.com>
2022-01-18 22:17:40 -0300

    Add Event.ts

---
+++ backend/src/entities/Event.ts
@@ -0,0 +1,32 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { User } from './User';
+
+@Entity('events')
+export class Event {
+   
+   @PrimaryGeneratedColumn('uuid')
+   id: string;
+
+   @Column()
+   title: string;
+
+   @Column()
+   description: string;
+
+   @UpdateDateColumn()
+   createdAt: Date;
+
+   @UpdateDateColumn()
+   initedAt: Date;
+
+   @UpdateDateColumn()
+   endAt: Date;
+
+   @ManyToOne(() => User, user => user.id)
+   @JoinColumn()
+   user: User;
+   @Column()
+   limit: number;
+
+   amountSubscriber: number;
+}
