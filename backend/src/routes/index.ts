607c16db12ecde8057d13924e7673a06334f48ee
fabio-ericle <fabioericle@gmail.com>
2022-01-18 22:20:57 -0300

    Update index.ts

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import eventRouter from './event.routes';
+import subscribeRouter from './subscribe.routes';
 import userRouter from './user.routes';
 
 const routes = Router();
 
 routes.use("/user", userRouter);
+routes.use("/event", eventRouter);
+routes.use(subscribeRouter);
 
 export default routes;
